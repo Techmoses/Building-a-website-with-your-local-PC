926c76263c6168d0832080800169bd8b3b75cf05
Techmoses <80067832+Techmoses@users.noreply.github.com>
2021-03-18 22:08:59 +0100

    Add files via upload

---
+++ Moses/index.php
@@ -0,0 +1,204 @@
+<html>
+<head>
+<title>SuperB moses</title>
+<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!---NavigationBar----->
+    <section id="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#top">HOME</a>
+      </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">ABOUT US</a>
+        </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#service">SERVICES</a>
+          </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="#contact">CONTACT</a>
+                </li>
+            </ul>
+            </div>
+    </section>
+    <!---slider----->
+    <div id="slider">
+        <div id="headerslider" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="headerslider" data-slide-to="0" class="active"></li>
+              <li data-target="headerslider" data-slide-to="1"></li>
+              <li data-target="#headerslider" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="img/mike3.jpg" alt="First slider">
+                    <div class="carousel-caption">
+                        <h5>Be a Professional with Programming</h5>
+                    </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="img/mike7.jpg" alt="Second slider">
+                    <div class="carousel-caption">
+                        <h5>Create Responsive Programming</h5>
+                    </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="img/mike6.jpg" alt="Third slider">
+                    <div class="carousel-caption">
+                        <h5>Buliding Face Detector</h5>
+                    </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="headerslider" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="headerslider" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+    </div>
+    <!---About------>
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-mid-6">
+                    <h2>About Us</h2>
+                    <div class="about-content">
+                        I Am Adewara Moses a programer i build a application and create a project topic,
+                        This is the site related to the programmer where you can get knowledge about different 
+                        things related to Information Technology. If you wish and want,  you can share your knowledge too. 
+                        Let me know if anyone wants to part of this programming Language and share their knowledge.
+                        And also watch more videos on How to create a face detector, Bulid a Application on webscram
+                        and Project topic. like this video and ask your questions in comment
+                        section, don't forget to subscrible Easy Tutorials YouTube Channel.
+                    </div>
+                    <button type="button" class="btn btn-primary">Read more
+                    </button>
+                    <div class="col-mid-6 skills-bar">
+                        <p>Application Creating leaflet webmaps with python and folium</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 80%;">80%</div>
+                        </div>
+                        <p>Application Building a website with python and flask</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 85%;">85%</div>
+                        </div>
+                        <p>Basic game Development</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 75%;">75%</div>
+                        </div>
+                        <p>Data Science</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 50%;">50%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!---services------>
+    <section id="service">
+        <div class="container">
+            <h1>Our Srevices</h1>
+            <div class="row services">
+                <div class="col-md-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-desktop"></i>
+                    </div>
+                    <h3>Web Development</h3>
+                    <p>Build with python and flask. The Flask-Script extension provides support for writing external scripts in Flask. This includes running a development server, a customised Python shell, scripts to set up your database, cronjobs, and other command-line tasks that belong outside the web application itself.</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-tablet"></i>
+                    </div>
+                    <h3>App Development</h3>
+                    <p>Dynamic Typing, Built-In Data Structures, Powerful Libraries, Frameworks, Community Support are just some of the reasons which make Python an attractive language for rapidly developing any sort of application. In this article, we will see some of the areas where Python excels in application development.. The following topics are to be covered in this article of Python Applications.</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-line-chart"></i>
+                    </div>
+                    <h3>Data Science</h3>
+                    <p>Python shines bright as one such language as it has numerous libraries and built in features which makes it easy to tackle the needs of Data science. In this tutorial we will cover these the various techniques used in data science using the Python programming language.</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <div class="icon">
+                        <i class="fa fa-paint-brush"></i>
+                    </div>
+                    <h3>Basic game Development</h3>
+                    <p>Python is a very versatile modern programming language and in this course you will learn basic game development using a python library called pygame. We will be creating a clone version of the very popular game called flappy bird that made a lot of money for it's developer few years ago.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!---promo--------->
+    <section id="promo">
+        <div class="container">
+            <img class="d-block w-100" src="img/promo.jpeg" alt="">
+            <h1>SuperB moses learn more and be professional with Python</h1>
+            <a href="#contact" class="btn btn-primary">Contact Us</a>
+        </div>
+    </section>
+    <!---Get in Touch---------->
+    <section id="contact">
+        <div class="container">
+            <h1>Get In Touch</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <form class="contact-from">
+                        <div class="from-group">
+                            <input type="text" class="form-control" placeholder="Your name">
+                        </div>
+                        <div class="from-group">
+                            <input type="Number" class="form-control" placeholder="Phone No">
+                        </div>
+                        <div class="from-group">
+                            <input type="Email" class="form-control" placeholder="Email id">
+                        </div>
+                        <div class="from-group">
+                            <textarea class="form-control" rows="4" placeholder=" Your Message"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">SEND MESSAGE</button>
+                    </form>
+                </div>
+                <div class="col-md-6 contact-info">
+                    <div class="follow"><b>Address:</b><i class="fa fa-map-marker"></i> Obalende, Machitn National Open University</div>
+                    <div class="follow"><b>Phone:</b><i class="fa fa-phone"></i>+234 8140098644</div>
+                    <div class="follow"><b>Email:</b><i class="fa fa-enveplope-o"></i>adewarasalvation56@gmail.com</div>
+                    <div class="follow"><label><b>Get social:</b></label>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-youtube">https://youtu.be/Hwwtr9Wq4q0</i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!---Footer---------->
+    <section id="footer">
+        <div class="container text-center">
+            <p>Made With <i class="fa fa-heart-o"></i> by Adewara Tutorials</p>
+        </div>
+    </section>
+    <!---Footer End---------->
+    <script src="js/smooth-scroll.js"></script>
+    <script>
+        var scroll = new SmoothScroll('a[href*="#"]');
+    </script>
+</body>
+</html>
